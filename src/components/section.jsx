f3534b6804d3b3acbe504efe8cdcd74449b8df19
ulugbekAbdullayev <ulu23503@gmail.com>
2025-02-14 00:41:12 +0500

    first commit

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -1,11 +1,18 @@
 
+import foto0 from "../photos/0.png"
+import foto1 from "../photos/1.png"
+import foto2 from "../photos/2.png"
+import foto3 from "../photos/3.png"
+import foto4 from "../photos/19.png"
+
+
 function ortaqism(){
    return (
     <section>
     <div className='container articles'>
       <div className='image_box'>
 
-        <img src="https://s3-alpha-sig.figma.com/img/fe0e/9c6d/c53ad70155193c56192deea27baa84fc?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VYImVR4BbOJLZ8z5LZAXpjsht1iOVeRPLO0uMvR9GmAfl02BOAfftNUglHg3gv7DtyWaVer8Oz0RY2nOodqVh98pOi4qhht4WfpQ04aStRus0uKWZepX8LUk0sbgKQHqyntU90a-jmg5AS2FBmUqliYFOio-ZflFitxwsdb8WSLVi1tNr0xdgnGbRFfSMZWO0mZ~AytUemCQo1TJ~iEVOA038A2WdTm539vklPwMRMm04Z5Xgi3BHT2Fw46jZSg8fjbA7uYp2J8p3FKZR-6d~L-JTIusbt8mx8GjV6OEnq5a4HaH1c1QKTqOWgoISfDvBwg0gJJBQ080t5ZZ2JHtzA__" alt="" />
+        <img src={foto0} alt="foto" />
 
       </div>
       <div className='titles'>
@@ -16,7 +23,7 @@ function ortaqism(){
           <br /><br /><br />
           <div className='title_bottom'>
             <div className='cooper'>
-              <img className='title_image' src="https://s3-alpha-sig.figma.com/img/7237/1d98/12e0d2d1d9fbccfb720a6e65527889a0?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fXige1hDrNS90Y7qQLi0csDZBHTTmlpzzsO0eJGwcYC-PtDmsgSseAyxUG0bazH684lbzRRQ5lAjt2eJvgk0ruWfFozJA43UygiAz9pgheAiCMMue3xgu70tPkwTbvGqWihXR4Nsww-cAaV47zxLyvOl55pViB~gOF2Sp97AXBp-hxjAmMudDd7DwSny41CmZDA9IQ2heLv8lmGEhGWXawNijki4Wq4rifUifQspBIHy0d-H1i0nD1C48EeyIcFPf5ULOWI4~Uv8zCrgkPIKOo1dlKGd87BsFM0XYU1Qy1szomy9RTrZTek8AExanIAD918hBv-47wb33qWDcwvrJw__" alt="face logo" />
+              <img className='title_image' src={foto4} alt="face logo" />
 
               Jane Cooper <br /> Jan 10, 2022
             </div>
@@ -45,7 +52,7 @@ function ortaqism(){
     <div className='container articles'>
       <div className='image_box'>
 
-        <img src="https://s3-alpha-sig.figma.com/img/ddf0/c8f6/6b97107fbd0d391736f99333b067073e?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dmFGSxPuAuWj2eJArCktalZmgAGPWrpOqQSwBvqHyDkleqmpg717ITwJvro6Fbpin9LxBlEAJxGn1DjOPlcE19v7nozinHVbZt-DeTx8eqIhJ5Ou-8t2v0FZGDr480ovdvJoaFvaognvA0CozZv0VWPfSmc8Rb6hC3AkMGqqQwVYJMVDx3syltH7kZM5wqBWWljv0wCv1lVqdWfgBLCC3reFv4OTt8BqmXRDA7wHAS5cyUrhqkA3talWnJmFOKfSEPq5hTPsWFltrylEp7VFqzTML0I~jyJeYOAUpqZYqXwjeSA8v-~2uatmVnhPkrCfFAkgxzaCW-p1jWaKT9LrUw__" alt="" />
+        <img src={foto1} alt="" />
 
       </div>
       <div className='titles'>
@@ -56,7 +63,7 @@ function ortaqism(){
           <br /><br /><br />
           <div className='title_bottom'>
             <div className='cooper'>
-              <img className='title_image' src="https://s3-alpha-sig.figma.com/img/7237/1d98/12e0d2d1d9fbccfb720a6e65527889a0?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fXige1hDrNS90Y7qQLi0csDZBHTTmlpzzsO0eJGwcYC-PtDmsgSseAyxUG0bazH684lbzRRQ5lAjt2eJvgk0ruWfFozJA43UygiAz9pgheAiCMMue3xgu70tPkwTbvGqWihXR4Nsww-cAaV47zxLyvOl55pViB~gOF2Sp97AXBp-hxjAmMudDd7DwSny41CmZDA9IQ2heLv8lmGEhGWXawNijki4Wq4rifUifQspBIHy0d-H1i0nD1C48EeyIcFPf5ULOWI4~Uv8zCrgkPIKOo1dlKGd87BsFM0XYU1Qy1szomy9RTrZTek8AExanIAD918hBv-47wb33qWDcwvrJw__" alt="face logo" />
+              <img className='title_image' src={foto4} alt="face logo" />
 
               Jane Cooper <br /> Jan 10, 2022
             </div>
@@ -87,7 +94,7 @@ function ortaqism(){
     <div className='container articles'>
       <div className='image_box'>
 
-        <img src="https://s3-alpha-sig.figma.com/img/9af0/6d30/8cb46f7c3b506aac5d7501835c0d1163?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WsbyYwxV2IH1Ddpmra5HarAVsrYev2U6wUJURDpgFA2EoMHVwkjDGgfpfwZhoQ4p6d5k4xKRYgJ-KGBIWNQtTCZWjwupkmk46XKpG1TklXcozt~gAWQYsxA-Sjci0eYJ3ICIpRYwqHqfiNQV58xcV3K9PyxQBv28josDlTNGyOwvS~meQK9DhTmvbq30kdxzF7meNMchUnk9i9aSF2C1oqctwRLsKbFJJ8RmiMKfMnJb5kRpBdKtROOM8lZS4nBga79pR04D27WMy~f53biVSvTbZhTjEyjUnMzAqsg7ce9Dw3xEQVMPvp6Bvpl8tycG5HksTSU4hJG6pW8eNInEHQ__" alt="" />
+        <img src={foto2} alt="" />
 
       </div>
       <div className='titles'>
@@ -98,7 +105,7 @@ function ortaqism(){
           <br /><br /><br />
           <div className='title_bottom'>
             <div className='cooper'>
-              <img className='title_image' src="https://s3-alpha-sig.figma.com/img/7237/1d98/12e0d2d1d9fbccfb720a6e65527889a0?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fXige1hDrNS90Y7qQLi0csDZBHTTmlpzzsO0eJGwcYC-PtDmsgSseAyxUG0bazH684lbzRRQ5lAjt2eJvgk0ruWfFozJA43UygiAz9pgheAiCMMue3xgu70tPkwTbvGqWihXR4Nsww-cAaV47zxLyvOl55pViB~gOF2Sp97AXBp-hxjAmMudDd7DwSny41CmZDA9IQ2heLv8lmGEhGWXawNijki4Wq4rifUifQspBIHy0d-H1i0nD1C48EeyIcFPf5ULOWI4~Uv8zCrgkPIKOo1dlKGd87BsFM0XYU1Qy1szomy9RTrZTek8AExanIAD918hBv-47wb33qWDcwvrJw__" alt="face logo" />
+              <img className='title_image' src={foto4} alt="face logo" />
 
               Jane Cooper <br /> Jan 10, 2022
             </div>
@@ -128,7 +135,7 @@ function ortaqism(){
     <div className='container articles'>
       <div className='image_box'>
 
-        <img src="https://s3-alpha-sig.figma.com/img/9b42/a733/3d70a7e1931ee7a8c6a55b3d6cbd435e?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SHnnQ6BX6O1XXj5xR0MpIaO5Nkb75p78rHg37fFmLqQwSn6HaMhx5f9W9Bpun6nMrbeVFFruExqXnLlril7d75mfeqkjy5sxxZzee-TzNLC0O6r9la9Sc4S~HH8qUBv0QKsvi8Z56vOnikVP5Ddw45lL~GaLGWmcZCEK4xyCyNb6a6Ci4-XB2LMwzEjOnWtFDT48zy7Rd7NQk7zOomspc8mdu3h36bH1fPDi61T0AYIqv44cvNjo7NIHxNAllzdlc3wCi6E3sCsMvIq06v6vexvTsJClCffbqVNut21Fp1S8iC-KTtu7miK7RGtG8nTRoiwTrshRp~NJHbBS7RGgZQ__" alt="" />
+        <img src={foto3} alt="" />
 
       </div>
       <div className='titles'>
@@ -139,7 +146,7 @@ function ortaqism(){
           <br /><br /><br />
           <div className='title_bottom'>
             <div className='cooper'>
-              <img className='title_image' src="https://s3-alpha-sig.figma.com/img/7237/1d98/12e0d2d1d9fbccfb720a6e65527889a0?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fXige1hDrNS90Y7qQLi0csDZBHTTmlpzzsO0eJGwcYC-PtDmsgSseAyxUG0bazH684lbzRRQ5lAjt2eJvgk0ruWfFozJA43UygiAz9pgheAiCMMue3xgu70tPkwTbvGqWihXR4Nsww-cAaV47zxLyvOl55pViB~gOF2Sp97AXBp-hxjAmMudDd7DwSny41CmZDA9IQ2heLv8lmGEhGWXawNijki4Wq4rifUifQspBIHy0d-H1i0nD1C48EeyIcFPf5ULOWI4~Uv8zCrgkPIKOo1dlKGd87BsFM0XYU1Qy1szomy9RTrZTek8AExanIAD918hBv-47wb33qWDcwvrJw__" alt="face logo" />
+              <img className='title_image' src={foto4} alt="face logo" />
 
               Jane Cooper <br /> Jan 10, 2022
             </div>
